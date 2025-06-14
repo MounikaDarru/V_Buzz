648a824bfbe94ed6853e9c7e44849e85a610d701
MounikaDarru <darru.mounika.123@gmail.com>
2025-06-14 21:17:12 +0530

    Home/Events

--- src/components/cards/EventCard.jsx
+++ src/components/cards/EventCard.jsx
@@ -6,54 +6,52 @@ import ellipse from "../../assets/Ellipse.png";
 const EventCard = ({ image, title, date, time, location, speakers = [] }) => {
   return (
     <div className="rounded-xl flex justify-between font-poppins gap-[15px] items-center">
-      <div className="w-[200px] h-[200px]">
+      <div className="md:w-[200px] md:h-[200px] sm:w-[200px] sm:h-[260px]">
         <img 
         src={image}
         alt={title}
-        className="w-[200px] h-[200px] object-cover rounded-[20px]"
+        className="md:w-[200px] md:h-[200px] sm:w-[200px] sm:h-[260px] object-cover rounded-[20px]"
         />
       </div>
 
-      <div className="flex shadow-custom-nav p-[30px] rounded-[20px] h-[200px] w-[925px]">
+      <div className="flex md:flex-row sm:flex-col shadow-custom-nav p-[30px] rounded-[20px] md:h-[200px] md:w-[925px] lg:w-[1000px] sm:w-[400px] sm:h-[260px]">
+        {/*Title and date and time and location */}
         <div>
-          {/*Title and date and time and location */}
-          <div className="flex flex-row gap-8 w-[400px] h-[30px] items-center border border-primary p-4 rounded-full">
+          <div className="flex flex-row gap-8 md:w-[380px] sm:w-[320px] h-[30px] items-center border border-primary p-4 rounded-full">
             <div className="flex items-center gap-2">
-              <FaCalendarAlt className="text-primary text-[16px]" />
-              <span className="text-black font-medium">{date}</span> 
+              <FaCalendarAlt className="text-primary md:text-[16px] sm:text-[12px]" />
+              <span className="text-black font-medium md:text-[16px] sm:text-[12px]">{date}</span> 
             </div>
             <div className="w-[2px] h-[20px] bg-primary"></div>
             <div className="flex items-center gap-2">
-              <FaClock className="text-primary text-[16px]" />
-              <span className="text-black font-medium">{time}</span> 
+              <FaClock className="text-primary md:text-[16px] sm:text-[12px]" />
+              <span className="text-black font-medium md:text-[16px] sm:text-[12px]">{time}</span> 
             </div>
           </div>
           <h1 className="w-[400px] text-[16px] font-bold text-primary mt-[15px]">{title}</h1>
-          <div className="flex items-center gap-2 mt-[24px]">
-            <FaMapMarkerAlt className="text-primary text-[16px]" />
-            <span className="text-black font-medium">{location}</span> 
+          <div className="flex items-center gap-2 mt-[24px] sm:mt-[15px]">
+            <FaMapMarkerAlt className="text-primary md:text-[16px] sm:text-[12px]" />
+            <span className="text-black font-medium md:text-[16px] sm:text-[12px]">{location}</span> 
           </div>
         </div>
-        <div className="w-[2px] h-[150px] bg-black/10 ml-[140px]"></div>
-        <div className="flex flex-col ml-[24px] py-[15px] w-[155px]">
-          {/*Register and speakers */}
-          <div>
-            <button className="h-12 border border-primary text-primary px-8 py-2 rounded-full text-sm font-bold self-start md:self-auto">
+        <div className="md:w-[2px] md:h-[150px] sm:w-[340px] sm:h-[2px] bg-black/10 md:ml-[150px] sm:mt-[10px]"></div>
+        {/*Register and speakers */}
+        <div className="flex md:flex-col sm:flex-row sm:ml-[24px] md:py-[15px] sm:mt-[15px] ">
+          <button className="border border-primary text-primary md:px-8 md:py-2 rounded-full md:text-[16px] sm:text-[12px] sm:px-6 sm:py-2 font-bold self-start md:self-auto">
               REGISTER
-            </button>
-            <div className="flex items-center mt-[34px]">
-              {speakers.map((speaker, index) => (
-                <div key={index} className="flex items-center ml-[-12px]">
-                  <img
-                    src={speaker.photo}
-                    alt={speaker.name}
-                    className="w-[32px] h-[32px] rounded-full"
-                  />
-                </div>
-              ))}
-              <img src={ellipse} className="ml-[-12px]"/>
-              <p className="text-black text-[16px] font-bold ml-[-24px]">Speakers</p>
-            </div>
+          </button>
+          <div className="flex items-center md:mt-[34px] sm:ml-[50px]">
+            {speakers.map((speaker, index) => (
+              <div key={index} className="flex items-center ml-[-12px]">
+                <img
+                  src={speaker.photo}
+                  alt={speaker.name}
+                  className="md:w-[32px] md:h-[32px] rounded-full"
+                />
+              </div>
+            ))}
+            <img src={ellipse} className="ml-[-12px]"/>
+            <p className="text-black text-[16px] font-bold ml-[-24px]">Speakers</p>
           </div>
         </div>
       </div>

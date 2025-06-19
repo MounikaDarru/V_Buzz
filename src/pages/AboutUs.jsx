42a29350d3199fb295e124f29469f7b79d41546e
MounikaDarru <darru.mounika.123@gmail.com>
2025-06-19 22:55:44 +0530

    About Us/Our Values

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import NavBar from '../components/NavBar'
 import Hero from '../components/about us/Hero'
+import OurValues from '../components/about us/Our Values'
 
 const AboutUs = () => {
   return (
     <>
         <NavBar />
         <Hero />
+        <OurValues />
     </>
   )
 }

94aaa016a20561051bea23ba87b592fe5f9f1cc6
MounikaDarru <darru.mounika.123@gmail.com>
2025-06-23 20:04:37 +0530

    Services/Hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import InstituteBoarding from "./pages/InstituteBoarding";
 import AboutUs from "./pages/AboutUs";
+import Services from "./pages/Services";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about-us" element={<AboutUs />} />
+      <Route path="/services" element={<Services />} />
       <Route path="/institute-boarding" element={<InstituteBoarding />} />
     </Routes>
   )
---
+++ src/pages/Services.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import NavBar from '../components/NavBar'
+import Hero from '../components/services/Hero'
+
+const Services = () => {
+  return (
+    <>
+        <NavBar />
+        <Hero />
+    </>
+  )
+}
+
+export default Services
